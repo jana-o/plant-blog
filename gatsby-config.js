22996e566aec1060f05c5d7b407bdc7a6d48e9a4
Steffi Friedrichs <steffriedrichs@Steffis-MacBook-Pro-2.local>
2020-11-22 23:40:46 +0100

    add favico

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
         background_color: `#fff`,
         theme_color: `#381696`,
         display: `standalone`,
-        icon: "src/images/icon.png",
+        icon: "static/favicon.ico",
       },
     },
     `gatsby-plugin-sass`, 
