d3b1f4349789232fa4aa11618f21fa5afcfc9699
Steffi Friedrichs <steffriedrichs@Steffis-MacBook-Pro-2.local>
2020-11-24 22:08:38 +0100

    first version of about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,6 @@
-import React from "react"
-import Helmet from "react-helmet"
-import Layout from "../components/layout"
+import React from "react";
+import Helmet from "react-helmet";
+import Layout from "../components/layout";
 
 const AboutPage = () => {
   return (
@@ -8,26 +8,25 @@ const AboutPage = () => {
       <Helmet>
         <title>About</title>
       </Helmet>
-
-      <div>
-        Schön, dass Du den Weg in unseren Zimmergarten gefunden hast. Hier gibt es von nützlichen Pflegetipps über persönliche Erfahrungen bis zu wissenswerten Informationen alles, was Pflanzenliebhaber interessiert.
-      </div>
-      <div>
-        <p>
-          Bei Fragen, Ideen, Anmerkungen schreibt uns unter: info@zimmergruen.com oder zimmergruenblog@gmail.com
-          Wir freuen uns von euch zu hören.
-        </p>
-        <p>
-          This page is powered by Netlify, NetlifyCMS and W3Layouts.
-        </p>
-
-      </div>
-      <div>
-        Impressum
-        Berlin/Göttingen, 2020
-        Stefanie Friedrich und Jana Orth
+      <div style={{padding: "5vh 0", lineHeight: "1.5", maxwidth: "50%"}}>
+        <div>
+          Schön, dass Du den Weg in unseren Zimmergarten gefunden hast. Hier
+          gibt es von nützlichen Pflegetipps über persönliche Erfahrungen bis zu
+          wissenswerten Informationen alles, was Pflanzenliebhaber interessiert.
+        </div>
+        <div style={{paddingtop: "20px"}}> 
+            Bei Fragen, Ideen, Anmerkungen schreibt uns unter:
+            info@zimmergruen.com oder zimmergruenblog@gmail.com Wir freuen uns
+            von euch zu hören.
+        </div>
+        <div style={{paddingtop: "50px"}}> 
+            This page is powered by Netlify, NetlifyCMS and W3Layouts.
+        </div>
+        <div style={{paddingtop: "80px"}}>
+          Impressum Berlin/Göttingen, 2020 Stefanie Friedrichs und Jana Orth
+        </div>
       </div>
     </Layout>
-  )
-}
-export default AboutPage
+  );
+};
+export default AboutPage;
