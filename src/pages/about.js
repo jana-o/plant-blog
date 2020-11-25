9185dcc2e9a01782ab5b4c049263816d55cbf152
Jana <jana.yj.orth@gmail.com>
2020-11-25 16:27:41 +0100

    minor about

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,22 +8,22 @@ const AboutPage = () => {
       <Helmet>
         <title>About</title>
       </Helmet>
-      <div style={{padding: "5vh 0", lineHeight: "1.5", maxwidth: "50%"}}>
+      <div style={{ padding: "5vh 0", lineHeight: "1.5", maxwidth: "50%" }}>
         <div>
           Schön, dass Du den Weg in unseren Zimmergarten gefunden hast. Hier
           gibt es von nützlichen Pflegetipps über persönliche Erfahrungen bis zu
           wissenswerten Informationen alles, was Pflanzenliebhaber interessiert.
         </div>
-        <div style={{paddingtop: "20px"}}> 
-            Bei Fragen, Ideen, Anmerkungen schreibt uns unter:
-            info@zimmergruen.com oder zimmergruenblog@gmail.com Wir freuen uns
-            von euch zu hören.
+        <div style={{ paddingtop: "20px" }}>
+          Bei Fragen, Ideen, Anmerkungen schreibt uns unter:
+          zimmergruenblog@gmail.com ~ Wir freuen uns
+          von euch zu hören.
         </div>
-        <div style={{paddingtop: "50px"}}> 
-            This page is powered by Netlify, NetlifyCMS and W3Layouts.
+        <div style={{ paddingtop: "50px" }}>
+          This page is powered by Netlify, NetlifyCMS and W3Layouts.
         </div>
-        <div style={{paddingtop: "80px"}}>
-          Impressum Berlin/Göttingen, 2020 Stefanie Friedrichs und Jana Orth
+        <div style={{ paddingtop: "80px" }}>
+          Impressum {"\n"}Berlin/Göttingen, 2020 {"\n"}Stefanie Friedrichs & Jana Orth
         </div>
       </div>
     </Layout>
