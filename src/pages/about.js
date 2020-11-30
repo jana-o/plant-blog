9bd2a46f01d9792b3a1efab7009b0dacb6a107c2
Steffi Friedrichs <steffriedrichs@Steffis-MacBook-Pro-2.local>
2020-11-30 22:29:55 +0100

    format impressum

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,24 +8,32 @@ const AboutPage = () => {
       <Helmet>
         <title>About</title>
       </Helmet>
-      <div style={{padding: "5vh 0", lineHeight: "1.5", maxwidth: "50%"}}>
-        <div>
-          Schön, dass Du den Weg in unseren Zimmergarten gefunden hast. Hier
-          gibt es von nützlichen Pflegetipps über persönliche Erfahrungen bis zu
-          wissenswerten Informationen alles, was Pflanzenliebhaber interessiert.
+      <div style={{padding: "5vh 0", lineHeight: "1.5", maxWidth: "50%"}}>
+            <div>
+                Schön, dass Du den Weg in unseren Zimmergarten gefunden hast. <br></br>
+                Hier gibt es von nützlichen Pflegetipps über persönliche Erfahrungen bis zu
+                wissenswerten Informationen alles, was Pflanzenliebhaber interessiert.
+            </div>
+            <div style={{paddingTop: "20px"}}> 
+                Bei Fragen, Ideen, Anmerkungen schreibt uns unter:<br></br>
+                info@zimmergruen.com oder zimmergruenblog@gmail.com Wir freuen uns
+                von euch zu hören.
+            </div>
+            <div style={{paddingTop: "50px"}}> 
+                This page is powered by Netlify, NetlifyCMS and W3Layouts.
+            </div>
         </div>
-        <div style={{paddingtop: "20px"}}> 
-            Bei Fragen, Ideen, Anmerkungen schreibt uns unter:
-            info@zimmergruen.com oder zimmergruenblog@gmail.com Wir freuen uns
-            von euch zu hören.
+        <div style={{padding: "5vh 0", lineHeight: "1.5", maxWidth: "50%"}}>
+            <h3>Impressum</h3>
+            <div style={{paddingTop: "20px", fontSize: "80%"}}>
+                Stefanie Friedrichs und Jana Orth <br></br>
+                10405 Berlin / 37073 Göttingen
+            </div>
+            <h4>Kontakt</h4>
+            <div style={{paddingTop: "20px", fontSize: "80%"}}>
+                zimmergruenblog@gmail.com<br></br>
+            </div>
         </div>
-        <div style={{paddingtop: "50px"}}> 
-            This page is powered by Netlify, NetlifyCMS and W3Layouts.
-        </div>
-        <div style={{paddingtop: "80px"}}>
-          Impressum Berlin/Göttingen, 2020 Stefanie Friedrichs und Jana Orth
-        </div>
-      </div>
     </Layout>
   );
 };
