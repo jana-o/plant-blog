f77f56c11544b7523c7d647f4490e2b6f701e0b2
Steffi Friedrichs <steffriedrichs@Steffis-MacBook-Pro-2.local>
2020-12-06 22:09:04 +0100

    add impressums page

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,9 +1,10 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
 
 export default (props) => (
   <nav className="navigation">
     {/* <Link to="/contact">Contact</Link> */}
     <Link to="/about">About</Link>
+    <Link to="/impressum">Impressum</Link>
   </nav>
-)
+);
--- src/pages/about.js
+++ src/pages/about.js
@@ -8,31 +8,34 @@ const AboutPage = () => {
       <Helmet>
         <title>About</title>
       </Helmet>
-      <div style={{display: "grid", gridTemplateColumns: "auto 30%"}}>
-        <div style={{padding: "5vh 0", lineHeight: "1.5", maxWidth: "50%", alignSelf: "end"}}>
-            <p>Wie schön, dass Du den Weg in unseren Zimmergarten gefunden hast!</p>   
-            <p>
-                Hier gibt es von nützlichen Pflegetipps über persönliche Erfahrungen bis zu
-                wissenswerten Informationen alles, was Pflanzenliebhaber interessiert.
-            </p>
-            <p> 
-                Bei Fragen, Ideen, Anmerkungen schreibt uns unter:<br></br>
-                <span style={{color: "#117538"}}>zimmergruenblog@gmail.com</span> <br></br>
-            </p>    
-            <p>Wir freuen uns von euch zu hören.</p>
+      <div style={{ display: "grid", gridTemplateColumns: "auto 30%" }}>
+        <div
+          style={{
+            padding: "5vh 0",
+            lineHeight: "1.5",
+            maxWidth: "60%",
+            alignSelf: "end",
+          }}
+        >
+          <p>
+            Wie schön, dass Du den Weg in unseren Zimmergarten gefunden hast!
+          </p>
+          <p>
+            Hier gibt es von nützlichen Pflegetipps über persönliche Erfahrungen
+            bis zu wissenswerten Informationen alles, was Pflanzenliebhaber
+            interessiert.
+          </p>
+          <p>
+            Bei Fragen, Ideen, Anmerkungen schreibt uns unter:<br></br>
+            <span style={{ color: "#117538" }}>
+              zimmergruenblog@gmail.com
+            </span>{" "}
             <br></br>
-            <br></br>
-            <p>This page is powered by Netlify, NetlifyCMS and W3Layouts.</p>
-        </div>
-        
-        <div style={{padding: "5vh 0", lineHeight: "1.5", alignSelf: "end"}}>
-        <h3>Impressum</h3>
-            <p>
-                Stefanie Friedrichs und Jana Orth <br></br>
-                10405 Berlin / 37073 Göttingen
-            </p>
-            <h4 style={{marginBottom: "5px"}}>Kontakt</h4>
-            <p>zimmergruenblog@gmail.com</p>
+          </p>
+          <p>Wir freuen uns von euch zu hören.</p>
+          <br></br>
+          <br></br>
+          <p>This page is powered by Netlify, NetlifyCMS and W3Layouts.</p>
         </div>
       </div>
     </Layout>
